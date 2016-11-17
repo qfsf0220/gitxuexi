5237a6a621597e6492c9eb76d28fbf7c17c97deb
qfsf0220 <371204418@qq.com>
2016-11-17 17:28:37 +0800

    test1

--- test001/src/sortarray.java
+++ test001/src/sortarray.java
@@ -2,9 +2,9 @@
 public class sortarray{
     public static void main(String[] args){
         int[] aaa= {432,345,3,123,34,565,67,234,221,1,2,3};
-        Arrays.sort(aaa);
-        for (int r :aaa){
-            System.out.println(r);
-        }
+//        Arrays.sort(aaa);
+//        for (int r :aaa){
+//            System.out.println(r);
+//        }
     }
 }
--- test001/src/test001.java
+++ test001/src/test001.java
@@ -4,5 +4,10 @@
 public class test001 {
     public static void main(String[] args){
         System.out.println("this is a test.");
+        int [] aaa={1,2,3,235345,123,423};
+        for(int i =0;i<aaa.length;i++){
+            System.out.println(i);
+
+        }
     }
 }
