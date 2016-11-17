0ca4ca93a0a99c37ea166931438e6f3c636ff1a0
qfsf0220 <371204418@qq.com>
2016-11-17 16:53:55 +0800

    switch

---
+++ test001/src/switchswitch.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class switchswitch {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter a num (1-10)");
+        String aa= in.nextLine();
+        switch(aa){
+            case "1":
+            System.out.println("1111");
+            break;
+            case "2":
+            System.out.println("2222");
+            break;
+            case "3":
+            System.out.println("3333");
+            break;
+            default:
+            System.out.println("out of range.");
+
+        
+        }
+    }
+}
