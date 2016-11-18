c2830ae65bdba7ab2c5bd476eaf227c1788b53b8
qfsf0220 <371204418@qq.com>
2016-11-18 15:33:22 +0800

    er

--- test001/src/test1118001.java
+++ test001/src/test1118001.java
@@ -4,7 +4,38 @@ package gitxuexi.test001.src;
  * Created by feng.qian on 2016/11/18.
  */
 public class test1118001 {
-    public  static  void  main(String[]args){
-        System.out.println("dada");
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            System.out.print("--");
+        }
+        System.out.print("\n");
+        long [] aaa = {12,324,235};
+        for (int i =0;i<aaa.length;i++){
+            System.out.println(aaa[i]);
+        }
+
+        int i =60;
+        if (i ==29)
+            System.out.println("qf");
+        else if (i>29 &&i<40)
+            System.out.println("laozong");
+        else {
+            {{System.out.println("xiaohuozi");}}
+            }
+
+
+        int [] myarray = new int [10];
+        for (i =1;i<myarray.length;i++){
+            myarray[i]=100*i;
+        }
+
+        for(i=0;i<myarray.length;i++){
+            System.out.println(myarray[i]);
+        }
+
+
+        }
+
+
     }
-}
