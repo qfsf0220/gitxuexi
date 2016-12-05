bf0a259a3fbf6560e359a893449b994e30781cd1
qfsf0220 <371204418@qq.com>
2016-12-05 18:19:19 +0800

    1205commit

---
+++ test001/src/test1202/thisisatest.java
@@ -0,0 +1,22 @@
+package test1202;
+
+/**
+ * this is a test for  test1204.com.qfsf0220.packagetest
+ *test1204.com.qfsf0220.packagetest
+ * @author feng.qian
+ * @create at 2016-12-05 18:01
+ */
+
+
+public class thisisatest {
+    public static void main(String[] args) {
+        say();
+    }
+
+    public  static  void say(){
+        System.out.println("this is a test!!");
+    }
+
+}
+
+
---
+++ test001/src/test1204/com/qfsf0220/packagetest.java
@@ -0,0 +1,20 @@
+package test1204.com.qfsf0220;
+
+import  test1202.thisisatest;
+/**
+ * test the package and import
+ *
+ * @author feng.qian
+ * @create at 2016-12-05 17:43
+ */
+
+public class packagetest {
+    public static void main(String[] args) {
+        System.out.println("xxxx");
+
+        thisisatest cc = new thisisatest();
+
+        cc.say();
+
+    }
+}
