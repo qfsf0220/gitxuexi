dd756005722a5d93c36cabcce784668e6e942a3e
qfsf <371204418@qq.com>
2016-12-07 23:24:59 +0800

    commit  from laptop

--- test001/src/test1207/fanxingtest04.java
+++ test001/src/test1207/fanxingtest04.java
@@ -1,7 +1,33 @@
 package test1207;
+/*设置多个泛型
+* */
+class gf<F,S>{
+    private S key;
+    private F value;
 
-/**
- * Created by Administrator on 2016/12/7.
- */
+    public S getKey() {
+        return key;
+    }
+
+    public F getValue() {
+        return value;
+    }
+
+    public void setValue(F value) {
+        this.value = value;
+    }
+
+    public void setKey(S key) {
+        this.key = key;
+    }
+}
+
+
 public class fanxingtest04 {
+    public static void main(String[]args){
+        gf<Integer,String> g=new gf<>();
+        g.setValue(99);
+        g.setKey("qf");
+        System.out.println(g.getKey()+g.getValue());
+    }
 }
