34d3085a8aaff5f8038272d1aa051f8a69b090e5
qfsf0220 <371204418@qq.com>
2016-12-08 15:20:07 +0800

    commit at  YP

---
+++ test001/src/test1208/test2.java
@@ -0,0 +1,15 @@
+package test1208;
+
+/**
+ * test2
+ *
+ * @author feng.qian
+ * @create at 2016-12-08 15:19
+ */
+
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
