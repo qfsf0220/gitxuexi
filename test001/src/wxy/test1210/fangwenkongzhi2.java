b60cc8fb054b190643a61878a8642a5139e048d4
qfsf <371204418@qq.com>
2016-12-10 23:02:32 +0800

    方法的重载  12-4

--- test001/src/wxy/test1210/fangwenkongzhi2.java
+++ test001/src/wxy/test1210/fangwenkongzhi2.java
@@ -2,6 +2,15 @@ package wxy.test1210;
 
 /**
  * Created by qfsf on 2016/12/10.
+ *
+
+ 父类中声明为public的方法在子类中也必须为public。
+
+ 父类中声明为protected的方法在子类中要么声明为protected，要么声明为public。不能声明为private。
+
+ 父类中默认修饰符声明的方法，能够在子类中声明为private。
+
+ 父类中声明为private的方法，不能够被继承。
  */
 class Dog2{
     private String name;
