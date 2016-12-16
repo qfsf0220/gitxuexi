ac6d60552ed3a1245e5b4d646c86b818ea59e956
qf <371204418@qq.com>
2016-12-16 23:54:43 +0800

    commit xh

---
+++ test001/src/wxy/test1216/testonmac.java
@@ -0,0 +1,10 @@
+package wxy.test1216;
+
+/**
+ * Created by qf on 2016/12/16.
+ */
+public class testonmac {
+    public static void main(String []args){
+        System.out.printf("this is a test \n");
+    }
+}
