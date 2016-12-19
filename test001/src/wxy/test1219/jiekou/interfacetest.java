62b4127d7ffd66e9b660ad82b6a506ab7deaf6c7
qfsf0220 <371204418@qq.com>
2016-12-19 18:31:46 +0800

    commit at  YP

---
+++ test001/src/wxy/test1219/jiekou/interfacetest.java
@@ -0,0 +1,16 @@
+package wxy.test1219.jiekou;
+
+/**
+ * 在抽象类中，可以包含一个或多个抽象方法；但在接口(interface)中，所有的方法必须都是抽象的，不能有方法体，它比抽象类更加“抽象”。
+ *
+ * 接口使用 interface 关键字来声明，可以看做是一种特殊的抽象类，可以指定一个类必须做什么，而不是规定它如何去做。
+ * @author feng.qian
+ * @create at 2016-12-19 18:12
+ */
+
+
+public class interfacetest {
+    public static void main(String[] args) {
+
+    }
+}
