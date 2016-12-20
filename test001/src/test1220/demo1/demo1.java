e58035d5935fe0d796d9a711b28bfcbaa4f89327
qfsf0220 <371204418@qq.com>
2016-12-20 17:05:16 +0800

    commit at  YP

--- test001/src/test1220/demo1/demo1.java
+++ test001/src/test1220/demo1/demo1.java
@@ -11,7 +11,7 @@ package test1220.demo1;
 
 public class demo1 {
     public static void main(String[] args) {
-        System.out.println(" 抽象类可以为部分方法提供实现，避免了在子类中重复实现这些方法，提高了代码的可重用性，这是抽象类的优势；而接口中只能包含抽象方法，不能包含任何实现。  ");
+        System.out.printf(" 抽象类A有两个子类B、C，由于A中有方法method2的实现，\n子类B、C中不需要重写method2方法，我们就说A为子类提供了公共的功能，\n或A约束了子类的行为。method2就是代码可重用的例子。\nA 并没有定义 method1的实现，也就是说B、C 可以根据自己的特点实现method1方法，这又体现了松散耦合的特性。 ");
     }
 
     public abstract class A{
