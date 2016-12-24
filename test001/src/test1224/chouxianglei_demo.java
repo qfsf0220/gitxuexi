93596cda859452626e0dccf254a0039cbf068d6a
qfsf <371204418@qq.com>
2016-12-24 22:17:46 +0800

    上转型 多态测试

--- test001/src/test1224/chouxianglei_demo.java
+++ test001/src/test1224/chouxianglei_demo.java
@@ -5,16 +5,30 @@ package test1224;
  */
 public class chouxianglei_demo {
     public static void main(String[] args) {
-        car cc = new policecar();
-        cc.sound();
-        cc = new amblance();
-        cc.sound();
+//        car cc = new policecar();
+//        cc.sound();
+//        cc = new amblance();
+//        cc.sound();
+        soundmaker  s =new soundmaker();
+        car c = new policecar();
+        s.laba(c);
+        c = new amblance();
+        s.laba(c);
+
+
     }
 }
 
 abstract  class car {
     abstract void sound();
 }
+
+class soundmaker{
+    void    laba(car c){
+        c.sound();
+    }
+}
+
 
 class  policecar extends car{
     @Override
