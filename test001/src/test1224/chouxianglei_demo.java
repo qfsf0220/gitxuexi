eba8056b56fb7963156fb2ab8d10b607e31104aa
qfsf <371204418@qq.com>
2016-12-24 22:09:45 +0800

    上转型 多态测试

--- test001/src/test1224/chouxianglei_demo.java
+++ test001/src/test1224/chouxianglei_demo.java
@@ -5,6 +5,27 @@ package test1224;
  */
 public class chouxianglei_demo {
     public static void main(String[] args) {
+        car cc = new policecar();
+        cc.sound();
+        cc = new amblance();
+        cc.sound();
+    }
+}
 
+abstract  class car {
+    abstract void sound();
+}
+
+class  policecar extends car{
+    @Override
+    void sound() {
+        System.out.println("wua  wua");
+    }
+}
+
+class amblance extends car{
+    @Override
+    void sound() {
+        System.out.println("didu  didu");
     }
 }
