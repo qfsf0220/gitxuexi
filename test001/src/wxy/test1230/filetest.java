5287b54643e15c16c70734cefd0e19610135767e
qfsf0220 <371204418@qq.com>
2016-12-30 10:49:54 +0800

    commit at  YP

--- test001/src/wxy/test1230/filetest.java
+++ test001/src/wxy/test1230/filetest.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 /**
  * @author feng.qian
  * @create at 2016-12-30 10:11
+使用 FileReader 类读取文件
+
+FileReader 类是 Reader 子类 InputStreamReader 类的子类，因此 FileReader 类既可以使用Reader 类的方法也可以使用 InputStreamReader 类的方法来创建对象。
+
 在使用 FileReader 类读取文件时，必须先调用 FileReader()构造方法创建 FileReader 类的对象，再调用 read()方法。FileReader 构造方法的格式为：
 public FileReader(String name);  //根据文件名创建一个可读取的输入流对象
 
@@ -21,6 +25,7 @@ public class filetest {
         String str = new String(a,0,num);
         System.out.println("读取的字符个数为："+num+",内容为：\n");
         System.out.println(str);
+        b.close();
 
     }
 }
---
+++ test001/src/wxy/test1230/filetest2.java
@@ -0,0 +1,18 @@
+package wxy.test1230;
+
+/**
+ * @author feng.qian
+ * @create at 2016-12-30 10:47
+使用 BufferedReader 类读取文件
+
+BufferedReader 类是用来读取缓冲区中的数据。使用时必须创建 FileReader 类对象，再以该对象为参数创建 BufferedReader 类的对象。BufferedReader 类有两个构造方法，其格式为：
+public BufferedReader(Reader in);  //创建缓冲区字符输入流
+public BufferedReader(Reader in,int size);  //创建输入流并设置缓冲区大小
+ */
+
+
+public class filetest2 {
+    public static void main(String[] args) {
+
+    }
+}
