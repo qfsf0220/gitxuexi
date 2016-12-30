3b753bc42941ae5f87301061abc7c020cbca97d7
qfsf0220 <371204418@qq.com>
2016-12-30 10:43:38 +0800

    commit at  YP

--- test001/src/test1229/threadtest2.java
+++ test001/src/test1229/threadtest2.java
@@ -8,6 +8,17 @@ package test1229;
 
 public class threadtest2 {
     public static void main(String[] args) {
+        Firstthread f = new Firstthread();
+        f.run();
+    }
+}
 
+class Firstthread extends Thread{
+    private int i;
+    public void  run(){
+        //当线程类继承Thread类时，直接使用this可以获得当前线程。
+        //Thread 对象的getName()返回当前线程的名字
+
+        System.out.println("当前线程的名字："+getName());
     }
 }
---
+++ test001/src/wxy/test1230/filetest.java
@@ -0,0 +1,26 @@
+package wxy.test1230;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+/**
+ * @author feng.qian
+ * @create at 2016-12-30 10:11
+在使用 FileReader 类读取文件时，必须先调用 FileReader()构造方法创建 FileReader 类的对象，再调用 read()方法。FileReader 构造方法的格式为：
+public FileReader(String name);  //根据文件名创建一个可读取的输入流对象
+
+需要注意的是，Java 把一个汉字或英文字母作为一个字符对待，回车或换行作为两个字符对待。
+ */
+
+
+public class filetest {
+    public static void main(String[] args) throws IOException {
+        char [] a = new char[1000];
+        FileReader b= new FileReader("D:\\tmp.txt");
+        int num = b.read(a);
+        String str = new String(a,0,num);
+        System.out.println("读取的字符个数为："+num+",内容为：\n");
+        System.out.println(str);
+
+    }
+}
