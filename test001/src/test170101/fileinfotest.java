60e51e9439fcb800ccae16b6f351a8efb4770b1d
qf <371204418@qq.com>
2017-01-01 23:17:24 +0800

    commit xh

---
+++ test001/src/test170101/fileinfotest.java
@@ -0,0 +1,27 @@
+package test170101;
+
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by qf on 2017/1/1.
+ */
+public class fileinfotest {
+    public static void main(String []args){
+        File ff = new File("/Users/qf/Downloads/android-sdk-macosx/skins/nexus_5x/layout");
+        System.out.println(ff.getName());
+        System.out.println(ff.getAbsolutePath());
+        System.out.println(ff.length());
+
+        File f2 = new File("/Users/qf/test","test.txt");
+        if (!f2.exists()){
+            try {
+                f2.createNewFile();
+                System.out.println("创建文件ok！");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
