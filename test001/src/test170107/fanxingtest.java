d01344a63c7b6563d96d6f6e96da33aa40c6ecd0
qf <371204418@qq.com>
2017-01-08 00:18:10 +0800

    commit xh

--- test001/src/test170107/fanxingtest.java
+++ test001/src/test170107/fanxingtest.java
@@ -2,6 +2,11 @@ package test170107;
 
 /**
  * Created by qf on 2017/1/7.
+ *
+ * 使用 class 名称 <泛型列表> 声明一个类  为了 和普通的类有所区别 这样的类成为泛型类
+ * class ShowObj<E>
+ *
+ * 其中 Showobj 是泛型类名称  E是泛型 ，E可以是 对象或接口，但是不能是基本类型数据
  */
 public class fanxingtest {
     public static void main(String []args){
@@ -16,7 +21,7 @@ public class fanxingtest {
 
 class Cat{
     @Override
-    public String toString() {
+    public String toString() {//泛型变量只能调用toString()方法
         return "this is a cat";
     }
 }
