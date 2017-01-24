8d2642296231289e060f2eb3039cccb9aa5492f3
qfsf0220 <371204418@qq.com>
2017-01-24 14:38:37 +0800

    commit at  YP

---
+++ test001/src/jktest/test170117/mianxiangduixiangtest.java
@@ -0,0 +1,18 @@
+package jktest.test170117;
+
+/**
+ * @author feng.qian
+ * @create at 2017-01-23 18:19
+ */
+
+
+public class mianxiangduixiangtest {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class subClass extends mianxiangduixiangtest{  //创建一个子类  所有的类都继承自 java.lang.Object
+
+}
