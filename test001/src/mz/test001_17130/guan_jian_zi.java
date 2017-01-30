f9ad89a76854e4aa2ee141db6b956bc24f998e10
qfsf <371204418@qq.com>
2017-01-30 22:59:04 +0800

    labtap

---
+++ test001/src/mz/test001_17130/guan_jian_zi.java
@@ -0,0 +1,18 @@
+package mz.test001_17130;
+
+/**
+ * Created by qfsf on 2017/1/30.
+ */
+public class guan_jian_zi {
+    public  static  void main(String[]args){
+        System.out.printf(
+                "1.标示符  由 字母， 下划线，美元符号和数字组成，长度不受限制。\n2.标示符的第一个字符不能是数字。\n3.标示符 不能是关键字，\n4.标示符不能是 true false  和 null\n5.标示符的字母区分大小写。\n");
+
+        System.out.println("--------------------");
+        System.out.println("---关键字---");
+
+        System.out.printf("●访问控制：\nprivate 私有的 protected 受保护的 public 公共的。\n●类，方法和变量修饰符：\nabstract 声明抽象 class 类  extends 扩充，继承  final 终极，不可改变的 implements  实现  interface 接口 native 本地   new  创建   static 静态  strictfp 严格,精准的  synchronized 线程，同步  transient 短暂  volatile 易失  \n●程序控制语句：\n break 跳出循环  continue 继续循环 return 返回 do 运行 while 循环 if如果 else  反之  for 循环 instance 实例  switch 开关 case 返回开关里的结果 default 默认  \n●错误处理：\ntry 捕获异常  catch 处理异常  finally 有没有异常都执行 throw 抛出一个异常对象  throws 声明一个异常可能被抛出    assert 断言 \n●包相关：\nimport 引入 package 包 \n●基本类型：\nboolean布尔类型   byte 字节类型   char 字符类型   double 双精度 float 浮点 int 整型 long 长整形   short 短整形   null 空    true 真   false 假   enum 枚举\n●变量引用：\nsuper 父类 超类    this 本类    void 无返回值 "   );
+
+
+    }
+}
