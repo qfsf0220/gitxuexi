7db09dcace50a2dbdd26b35ce2009d5eea114b77
qfsf <371204418@qq.com>
2017-02-27 22:32:11 +0800

    上转型 多态测试

---
+++ test001/src/testtemp/testtemp001.java
@@ -0,0 +1,11 @@
+package testtemp;
+
+/**
+ * Created by qfsf on 2017/2/27.
+ */
+public class testtemp001 {
+    public static void main(String[] args) {
+        System.out.printf("this is a test.\n");
+
+    }
+}
