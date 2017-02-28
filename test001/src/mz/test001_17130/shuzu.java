ed278c19985a83f6b681f6de5f923bd584de435e
qf <371204418@qq.com>
2017-02-28 23:29:50 +0800

    commit xh

---
+++ test001/src/mz/test001_17130/shuzu.java
@@ -0,0 +1,18 @@
+package mz.test001_17130;
+
+/**
+ * Created by qf on 2017/2/28.
+ */
+public class shuzu {
+    public static void main(String [] args){
+        float [] name;//声明一个一维数组
+        name = new float[8]; //8代表数组长度
+//float [] name = new float[8];
+
+        float [] [] name2;//声明一个二维数组
+        name2 = new float[3][4]; //3个长度为4的一维数组
+        // name2[0][0~3] name2[1][0~3] name2[2][0~3]
+//int [][] xx= new int[3][4];
+
+    }
+}
