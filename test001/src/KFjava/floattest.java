a31e5624219124d26bef6c38b2e81070c19e6503
qfsf0220 <371204418@qq.com>
2017-03-01 16:29:53 +0800

    commit at  YP

---
+++ test001/src/KFjava/floattest.java
@@ -0,0 +1,29 @@
+package KFjava;
+
+import java.util.DoubleSummaryStatistics;
+
+/**
+ * @author feng.qian
+ * @create at 2017-03-01 15:57
+ */
+
+
+public class floattest {
+    public static void main(String[] args) {
+        float fa = 5.55123456f;
+        System.out.println(fa);//这里输出显示为 5.5512347
+        double da = 0.0;
+        double c = Double.NEGATIVE_INFINITY;
+        float d = Float.NEGATIVE_INFINITY;
+        System.out.println( c == d ); //true 说明float 和double的负无穷大一样的。
+        System.out.println(da/da);//x 除以 0.0 将出现非数
+        //两个非数之间是不相等的
+        System.out.println(da/da == Float.NaN);
+        //所有正无穷大都是相等的
+        System.out.println(6.0/0 == 555.0/0);
+        //负数除以0.0等到负无穷大
+        System.out.println(-8/da);
+        //整数除以0 （不是 0.0） 会出现零除异常
+        //System.out.println(0/0);
+    }
+}
