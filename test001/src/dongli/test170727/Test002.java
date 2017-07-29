b629eda496499771e0a926b7b18ddd2e57880a19
qf <371204418@qq.com>
2017-07-30 00:08:07 +0800

    commit xh

--- test001/src/dongli/test170727/Test002.java
+++ test001/src/dongli/test170727/Test002.java
@@ -8,11 +8,26 @@ public class Test002 {
         TestTest001 tt =new TestTest001();
         System.out.println(tt.getClass());
         System.out.println(tt.a());
+        tt.b();
+        Test002.a();
+        Test002 t = new Test002();
+        t.aa();
+    }
+
+     static private void  a(){
+        System.out.println("test");
+    }
+    static void aa(){
+        System.out.println("test2");
     }
 }
 
 class TestTest001{
     public  int a(){
         return 123;
+    }
+
+    void b(){
+        System.out.println("test3");
     }
 }
