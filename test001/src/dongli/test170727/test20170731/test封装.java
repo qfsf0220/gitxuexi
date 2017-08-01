4271bce0d41a4cc13034815a56fabc570be9f3c9
qf <371204418@qq.com>
2017-08-01 09:35:38 +0800

    commit at sum

--- test001/src/dongli/test170727/test20170731/test封装.java
+++ test001/src/dongli/test170727/test20170731/test封装.java
@@ -1,6 +1,19 @@
 package dongli.test170727.test20170731;
 
 public class test封装 {
+
+    public static void  main(String[]a){
+        asd t = new asd();
+        //t.name="xxx";//注意这里private的限制 作用域仅仅是同一个class中。
+        t.setName("张总1");
+        if(t.getName()!=null)
+            System.out.println(t.getName());
+
+    }
+
+}
+
+class asd{
     private String name;
     public void setName(String name) {
         if (name != "张总"){
@@ -13,13 +26,5 @@ public class test封装 {
 
     public String getName() {
         return name;
-    }
-
-    public static void  main(String[]a){
-        test封装 t = new test封装();
-        t.setName("张总1");
-        if(t.getName()!=null)
-        System.out.println(t.getName());
-
     }
 }
