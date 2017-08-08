f0aa376b030dd561ad3b0e9058ff3d98b44715a0
qf <371204418@qq.com>
2017-08-08 11:44:15 +0800

    commit at sum

--- test001/src/wxy/sumsc/WrapperClass.java
+++ test001/src/wxy/sumsc/WrapperClass.java
@@ -24,20 +24,39 @@ class IntToString{
                 System.out.printf("字符串:%s 无法转换成数字\n",i);
             }
         }
-
+    }
+}
+class StringToInt{
+    public static void main(String[]a){
+        int m = 500;
+        String sm = Integer.toString(m);
+        System.out.printf("字符串：%s",sm);
     }
 }
 
 
 
-class Demo{
+class Demo{  //手动装箱 拆箱
     public  static void main(String[]a){
         int m = 500;
         Integer intttt = new Integer(m);//手动装箱
         int n = intttt.intValue();//手动拆箱
+        int t = intttt;//自动拆箱。
         System.out.println("n="+n);
 
         Integer inttt2 = new Integer(500);
         System.out.println("inttt 和 intttt2 一样?"+ intttt.equals(inttt2));
+    }
+}
+
+class Demo2{ //自动装箱  拆箱。
+    public static  void main(String[]args){
+        int m = 500;
+        Integer objint = m;//自动装箱
+        int n  = objint;//自动拆箱
+        System.out.printf("int n %d:",n);
+
+        Integer objint2 = 500;
+        System.out.println("objint 等价于 objint2?"+objint.equals(objint));
     }
 }
