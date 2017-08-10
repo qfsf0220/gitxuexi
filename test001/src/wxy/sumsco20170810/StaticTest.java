324bb89777de8461fc17869792d9860fa4f8a289
qf <371204418@qq.com>
2017-08-10 16:05:31 +0800

    commit at sum

--- test001/src/wxy/sumsco20170810/StaticTest.java
+++ test001/src/wxy/sumsco20170810/StaticTest.java
@@ -1,5 +1,9 @@
 package wxy.sumsco20170810;
 
+import static java.lang.Math.random;
+import static java.lang.System.*;
+/*对于使用频繁的静态变量和静态方法，可以将其静态导入。静态导入的好处是可以简化一些操作，例如输出语句 System.out.println(); 中的 out 就是 System 类的静态变量，可以通过 import static java.lang.System.*; 将其导入，下次直接调用 out.println() 就可以了。*/
+
 /**
 静态变量和静态方法的总结：
    ● 一个类的静态方法只能访问静态变量；
@@ -12,6 +16,7 @@ package wxy.sumsco20170810;
 
  */
 public class StaticTest {
+
     static int i;
     int j ;
 
@@ -44,5 +49,9 @@ public class StaticTest {
        System.out.println("i:"+i);
        new StaticTest().test();//匿名调用方法
 
+       for(int i =1;i<=10;i++){
+           out.println(random());
+       }
    }
 }
+
