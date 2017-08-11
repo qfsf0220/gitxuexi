2369ebb5316795f6472a4f99a7589bdbc7d27866
qf <371204418@qq.com>
2017-08-11 17:51:53 +0800

    commit at sum

---
+++ test001/src/wxy/sumscop20170811/InterfaceUsedbyObj.java
@@ -0,0 +1,27 @@
+package wxy.sumscop20170811;
+
+/**
+ * Created by Administrator on 2017/8/11.
+ */
+public class InterfaceUsedbyObj {
+    public  void test1(A a){
+        a.dosome();
+    }
+    public static void main(String[]a){
+        InterfaceUsedbyObj ii = new InterfaceUsedbyObj();
+        A aa = new B();
+        ii.test1(aa);
+    }
+
+}
+
+
+interface A {
+    public int dosome();
+}
+class B implements  A{
+    public  int dosome(){
+        System.out.println(" this is class B");
+        return 668;
+    }
+}
