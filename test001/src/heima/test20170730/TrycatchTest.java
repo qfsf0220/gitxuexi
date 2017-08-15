a687d79bc8d37c0bf559bae01797cfd0a4d6eb03
qfsf <371204418@qq.com>
2017-08-15 23:50:10 +0800

    test commit

---
+++ test001/src/heima/test20170730/TrycatchTest.java
@@ -0,0 +1,20 @@
+package heima.test20170730;
+
+/**
+ * Created by qfsf on 2017/8/15.
+ */
+class TrycatchTest {
+    public static void main(String[]a){
+        Demo d = new Demo();
+        int x =d.div(5,4);
+        System.out.println(x);
+
+        System.out.println("这里需要打印。");
+    }
+}
+
+class Demo{
+    int div(int a,int b){
+        return a/b;
+    }
+}
