5165dbd0d996233203fcac405b8306e331cbf2bf
qf <371204418@qq.com>
2017-08-18 16:02:40 +0800

    commit at sum

--- test001/src/heima/test0818/mianjiException.java
+++ test001/src/heima/test0818/mianjiException.java
@@ -4,10 +4,6 @@ import KFjava.test20170519.Shape_test;
 
 import javax.xml.stream.util.StreamReaderDelegate;
 
-/**
- * Created by Administrator on 2017/8/18.
- */
-
 class NovalueException extends RuntimeException{
     NovalueException(String msg){
         super(msg);
@@ -34,15 +30,32 @@ class Rec implements Shape{
     }
 }
 
+class Circle implements Shape{
+    private int r;
+    public static final double PI = 3.1416; // static 这个数据为共享数据 子类的话这个都能用到。public让其他类都能读到 final 不修改。
+    Circle(int r){
+        if (r<=0){
+            throw new NovalueException("弄好啊，小于0的数值，不对。");
+        }
+        this.r =r;
+    }
+    public void getArea() {
+        System.out.println(PI*r*r);
+    }
+}
+
 public class mianjiException {
     public static void main(String []a){
 
         //try{
-            Rec r = new Rec(5,-6);
+            Rec r = new Rec(5,6);
             r.getArea();
         //}catch(NovalueException e){
         //    e.printStackTrace();
         //}
+            Circle c = new Circle(6);
+            c.getArea();
+
         System.out.println("end.");
     }
 }
