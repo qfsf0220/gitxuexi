b2292499b58debd3211904a61983184f3603e190
qf <371204418@qq.com>
2017-08-23 18:00:22 +0800

    commit at sum

--- test001/src/shangxue/test0823/Visualdata.java
+++ test001/src/shangxue/test0823/Visualdata.java
@@ -27,17 +27,17 @@ public class Visualdata {
 //            System.out.println(week);
             int daymax = cal.getActualMaximum(Calendar.DATE);
 //            System.out.println(daymax);
-            System.out.println("         "+time);
-            System.out.println("日\t一\t二\t三\t四\t五\t六");
+            System.out.println("\t\t\t\t\t"+time);
+            System.out.println("日\t\t一\t\t二\t\t三\t\t四\t\t五\t\t六");
             for (int i=0;i<week-1;i++){
-                System.out.print('\t');
+                System.out.print("\t\t");
             }
 
             for (int i = 1; i <= daymax; i++) {
                 if(i ==day){
                     System.out.printf("<%s>\t",i);
                 }else {
-                    System.out.print(i + "\t");
+                    System.out.print(i + "\t\t");
                 }
                 int w= cal.get(Calendar.DAY_OF_WEEK) ;
                 if (w==Calendar.SATURDAY) {
