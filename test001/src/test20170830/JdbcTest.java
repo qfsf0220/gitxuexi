3ef801e40ef70bf281f2050e74b0f04fb6b61e11
qf <371204418@qq.com>
2017-08-30 16:16:29 +0800

    commit at sum

---
+++ test001/src/test20170830/JdbcTest.java
@@ -0,0 +1,42 @@
+package test20170830;
+
+import java.sql.*;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+public class JdbcTest {
+    private  static final String URL = "jdbc:mysql://qfsf0220.win:3306/sspanel";
+    private static final String NAME = "root";
+    private static final  String PASSWORD = "1234";
+
+    public static void main(String[] args) throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+//            System.out.println("class add ok");
+        } catch (ClassNotFoundException e) {
+            System.out.println("a..o...");
+            e.printStackTrace();
+        }
+
+        Connection con1 = DriverManager.getConnection(URL,NAME,PASSWORD);
+        Statement state =con1.createStatement();
+        ResultSet rs = state.executeQuery("SELECT  * FROM user");
+        while (rs.next()){
+            System.out.print(rs.getInt(1)+"\t");
+            System.out.print(rs.getString(2)+"\t");
+            System.out.print(rs.getString(3)+"\t");
+            System.out.print(rs.getString(4)+"\t");
+            System.out.print(rs.getString(5)+"\t");
+            System.out.println();
+
+        }
+
+
+
+
+
+    }
+
+}
+
