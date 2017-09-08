65d9f22a2c8c047bf10cfe50169fce3e76d83bf3
qf <371204418@qq.com>
2017-09-08 16:50:44 +0800

    commit at sum

--- test001/src/shangxue/test0908/ExceptionTest.java
+++ test001/src/shangxue/test0908/ExceptionTest.java
@@ -1,14 +1,24 @@
 package shangxue.test0908;
 
+import KFjava.test20170523.ParametersTrans;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import sun.net.TelnetProtocolException;
+
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import  java.lang.String;
+import java.text.ParseException;
 
 /**
  * Created by Administrator on 2017/9/8.
  */
+
+//3个方式。 1 try catch 2 方法throws  3 方法体内 手动 throw
 public class ExceptionTest {
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
         //nullpoint
         ttttt t  = null;
         if (t != null) { //手动处理 nullpoint 错误
@@ -20,6 +30,9 @@ public class ExceptionTest {
              fr  =new FileReader("e:\\qftest");
             char c = (char) fr.read();
             char c2 = (char) fr.read();
+            String frstr = fr.toString();
+            System.out.println(frstr);
+
             System.out.println(c+""+c2);
         }catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -33,13 +46,36 @@ public class ExceptionTest {
             }
         }
 
+        File f = new File("e://t.txt");
+
+        if (!f.exists()){
+            try {
+                throw new FileNotFoundException(); //手动抛出异常，使用不多。
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-
 
     class ttttt {
         public void tfunc(){
             System.out.println("123123");
         }
     }
+
+//子类抛出异常不能大于父类抛出异常
+    class aa{
+        public void fa()throws IOException{ }
+    }
+    class bb extends aa{
+        public void fa()throws FileNotFoundException{  }
+    }
+    class cc extends aa{
+//        public void fa()throws IOException,ParseException {} //这个ParseException错误不是子类
+    }
+    class dd extends  aa{
+//        public  void fa()throws  Exception{} //这个是Exception 是IOException的父类也是错误。
+    }
+
+
 }
