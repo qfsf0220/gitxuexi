db6086eee5baeb069c5018bade69f671cb598c22
qfsf <371204418@qq.com>
2017-09-24 02:08:38 +0800

    test commit

--- test001/src/CreateQRcode.java
+++ test001/src/CreateQRcode.java
@@ -2,11 +2,14 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.pdf417.decoder.ec.ErrorCorrection;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.google.zxing.client.j2se.*;
 
+
+import java.io.File;
+import java.nio.file.Path;
 import java.util.HashMap;
 
 
@@ -18,17 +21,19 @@ public class CreateQRcode{
         int width =300;
         int height = 300;
         String format = "png";
-        String content = "qfsf0220.win";
+        String content = "臭臭阿宝宝囡囡啊。";
 
         HashMap hints = new HashMap();
         hints.put(EncodeHintType.CHARACTER_SET,"utf-8");
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
         hints.put(EncodeHintType.MARGIN,2);
 
-
         try {
             BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height, hints);
 
+            Path path =new File("E:/tes.png").toPath();
+
+            MatrixToImageWriter.writeToPath(bitMatrix,format,path);
 
                    } catch (Exception e) {
             e.printStackTrace();
