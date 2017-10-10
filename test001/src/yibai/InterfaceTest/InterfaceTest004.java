6263f0a7dd4c3ab19019833f7791f981f181f38b
qf <371204418@qq.com>
2017-10-10 10:08:18 +0800

    commit at sum

--- test001/src/yibai/InterfaceTest/InterfaceTest004.java
+++ test001/src/yibai/InterfaceTest/InterfaceTest004.java
@@ -17,12 +17,22 @@ interface A2{
     int a2add(int n1, int n2);
 }
 
-class MM implements A1,A2{
+class MM implements A1,A2{//实现所有的接口
     public int a1add(int n1, int n2){
         return n1+n2;
     }
     public int a2add(int n1, int n2){
         return  n1+n2;
+    }
+}
+//如果实现部分接口的方法 必须要声明为abstract
+interface A3{
+    int a3add(int a,int b);
+    int a3subreact(int a,int b);
+}
+abstract class M2 implements A3{
+    public int a3add(int a,int b){
+        return a+b;
     }
 
 }
