7525b467e0dedca9c245bd585ad49670a7fea3f0
qf <371204418@qq.com>
2017-10-16 17:57:52 +0800

    commit at sum

---
+++ test001/src/yibai/ThreadTest/ThreadCallable002.java
@@ -0,0 +1,68 @@
+package yibai.ThreadTest;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+public class ThreadCallable002 {
+    public static void main(String[] args) {
+        bbb();
+
+    }
+
+    static void aaa() {
+        FutureTask<String> ff = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return String.valueOf("sfsd");
+            }
+        });
+        new Thread(ff).start();
+        String result;
+        try {
+            result = ff.get();
+            System.out.println(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void bbb() {
+        ExecutorService threadPoolExe = Executors.newCachedThreadPool();
+        Future<Integer> fff = threadPoolExe.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                for (int i = 1; i <= 5; i++) {
+                    System.out.println(Thread.currentThread().getName());
+
+                }
+                return new Random().nextInt(100);
+            }
+
+        });
+        threadPoolExe.shutdown();//如果不再需要新任务，请适当关闭threadPoolExecutor并拒绝新任务
+        try
+
+        {
+            for(int i = 1;i<=5;i++)
+            System.out.println(fff.get());
+        } catch (
+                InterruptedException e)
+
+        {
+            e.printStackTrace();
+        } catch (
+                ExecutionException e)
+
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
