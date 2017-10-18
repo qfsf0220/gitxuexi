7ba4fe6a2ce082b6942bba25235f81b83466f422
qf <371204418@qq.com>
2017-10-18 13:53:07 +0800

    commit at sum

--- test001/src/KFjava/SystemTest.java
+++ test001/src/KFjava/SystemTest.java
@@ -3,10 +3,7 @@ package KFjava;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Administrator on 2017/10/17.
@@ -21,9 +18,17 @@ public class SystemTest {
         System.out.println(System.getProperty("os.name"));
 
         Properties pt =System.getProperties();
-        pt.store(new FileOutputStream("E://props.txt"),"System Properties");
+//        pt.store(new FileOutputStream("E://props.txt"),"System Properties");
 
+        Map<Integer, String> stringMap = new HashMap<>();
+        stringMap.put(1, "aa");
+        stringMap.put(2, "bb");
+        stringMap.put(3, "cc");
+        stringMap.put(4, "dd");
+        stringMap.put(5, "ee");
 
+        System.out.println(stringMap.get(0));
+        Iterator<Integer> stringIterator = stringMap.keySet().iterator();
     }
 
 
