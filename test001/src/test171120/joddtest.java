3ace88193fc503093932de66947daa89e2e2a384
qf <371204418@qq.com>
2017-11-21 18:34:55 +0800

    commit at sum

--- test001/src/test171120/joddtest.java
+++ test001/src/test171120/joddtest.java
@@ -4,15 +4,62 @@ package test171120;
  * Created by Administrator on 2017/11/20.
  */
 
-import javax.swing.*;
+
+import jdk.nashorn.internal.scripts.JD;
+import jodd.datetime.JDateTime;
+import jodd.datetime.Period;
 import  jodd.format.Printf;
+
+import java.sql.Timestamp;
+import jodd.datetime.DateTimeStamp;
+import jodd.mail.Email;
+import jodd.util.StringUtil;
+import jodd.util.SystemUtil;
+
+import java.net.StandardSocketOptions;
+
+import java.util.Calendar;
+
 public class joddtest {
     public static void main(String[] args) {
-        testprint();
+       testprint();
+       joddtest jd = new joddtest();
+       jd.dateTest();
+       jd.sendEmail();
     }
 
     private static void testprint(){
         Printf.str("%04d", 1666);
         Printf.str("%f", 0.5);
     }
+    private void dateTest(){
+        JDateTime jdt = new JDateTime(System.currentTimeMillis());
+        jdt.setFormat("YYYY年MM月DD日 hh:mm:ss");
+        System.out.println("----");
+        System.out.println(new JDateTime(2012, 2, 2));
+        System.out.println(new JDateTime(System.currentTimeMillis()));
+        System.out.println(System.currentTimeMillis());
+        jdt.addHour(1);
+        System.out.println(jdt);
+        JDateTime jdt2 = new JDateTime(1988, 7, 02, 10, 1, 1,1);
+        System.out.println(jdt2);
+        Calendar c =jdt.convertToCalendar();
+
+        System.out.println(jdt.convertToSqlTimestamp());
+
+
+        System.out.println("不知不觉，你已经活了"+new Period(jdt,jdt2).getDays()+"天");
+        System.out.println((360*16));
+        System.out.println(System.currentTimeMillis());
+    }
+
+    private void strTest(){
+
+    }
+
+    private void sendEmail(){
+        Email email = Email.create().from("qq@qq.com").to("82970496@qq.com").subject("hello").addText("this is a test");
+    }
+
+
 }
