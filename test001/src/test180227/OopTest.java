3d2668bf089d75f4a31a879ee2d1e1d85fc77911
qf <371204418@qq.com>
2018-03-05 17:19:32 +0800

    commit at sum

--- test001/src/test180227/OopTest.java
+++ test001/src/test180227/OopTest.java
@@ -4,10 +4,46 @@ package test180227;
  * Created by Administrator on 2018/3/2.
  */
 public class OopTest {
+    OopTest(){
+        System.out.println("这个是Oooptest的无参构造方法。");
+    }
+    private int numx ;
     static int num3 =4;
+    static int b = 1234123;
+    int c = 678;
     int printnum3(){
         return num3;
     }
+    public  int getNumx(){
+        return numx;
+    }
+    public  void setNumx(int  numx){
+        this.numx = numx;
+        this.messagePrint();
+    }
+
+    public void messagePrint(){
+        System.out.println("this is a message.");
+    }
+
+    class InnerClass{
+        int numx = 5;
+        void printinner(){
+            System.out.println("this is a inner class"+" inner numx:"+numx+" outer numx:"+OopTest.this.numx); //内部类访问外部类的成员变量
+        }
+    }
+
+    static  class staticInnerClass{
+        int b = 222;
+        public  void printinner2(){
+            System.out.println("inner int b:"+b+"outer int b"+OopTest.b);
+            System.out.println("outer c:"+ new OopTest().c);//如果 内部静态类需要访问外部非静态成员 需要 new 外部类().成员 的方式访问。
+        }
+    }
+
+
+
+
     public static void  main(String[] args) {
 
         Thisistest tt = new Thisistest();
@@ -19,7 +55,17 @@ public class OopTest {
         System.out.println(OopTest.num3);
         OopTest ot = new OopTest();
         ot.printnum3();
+        ot.setNumx(123);
+        System.out.println("get  numx:"+ot.getNumx());
+        // 创建，调用内部类 需要通过 外部类对象 调用内部类对象。
+        InnerClass ic = ot.new InnerClass();
+        ic.printinner();
+        //直接创建内部类的对象
+        staticInnerClass staticinner = new staticInnerClass();
+        staticinner.printinner2();
 
+        funcinclassOuter fo = new funcinclassOuter();
+        fo.show();
 
     }
 }
@@ -48,6 +94,9 @@ class Thisistest{
         System.out.println("这个有返回值类型 void的  就不是构造函数了。");
     }
 
+
+
+
     static void  staticfun(){
         System.out.println("this is a static function");
     }
@@ -56,3 +105,25 @@ class Thisistest{
     }
 
 }
+
+class funcinclassOuter {
+    void show (){
+        int a = 1115;
+        int b = 222;
+        class funcinclassInner{
+            int c = 333;
+            void printt(){
+                System.out.println("a:"+a + " c:"+c);
+            }
+        }
+        funcinclassInner fi = new funcinclassInner();
+        fi.printt();
+    }
+
+    public static void main(String[] args) {
+        funcinclassOuter fo = new funcinclassOuter();
+        fo.show();
+    }
+}
+
+
